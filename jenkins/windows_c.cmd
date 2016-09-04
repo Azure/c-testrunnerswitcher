4c0b69bbebccb70d0e3d7b5f7e6baa811a079a41
dcristoloveanu <dcristo@microsoft.com>
2016-09-03 20:24:32 -0700

    Initial testrunner switcher checkin

---
+++ jenkins/windows_c.cmd
@@ -0,0 +1,25 @@
+@REM Copyright (c) Microsoft. All rights reserved.
+@REM Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+setlocal
+
+set build-root=%~dp0..
+rem // resolve to fully qualified path
+for %%i in ("%build-root%") do set build-root=%%~fi
+
+rmdir /s %build-root%\cmake
+mkdir %build-root%\cmake
+if errorlevel 1 goto :eof
+
+cd %build-root%\cmake
+cmake ..
+
+msbuild /m testrunnerswitcher.sln /p:Configuration=Release
+if errorlevel 1 goto :eof
+msbuild /m testrunnerswitcher.sln /p:Configuration=Debug
+if errorlevel 1 goto :eof
+
+ctest -C "debug" -V
+if errorlevel 1 goto :eof
+
+cd %build-root%

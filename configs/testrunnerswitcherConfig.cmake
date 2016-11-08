a33974e43302c5fb652b2267e1e1df85a34de6f1
andrew-buckley <andrewbuckley@outlook.com>
2016-11-08 09:59:56 -0800

    Adds copyright header to new cmake file.

--- configs/testrunnerswitcherConfig.cmake
+++ configs/testrunnerswitcherConfig.cmake
@@ -1,3 +1,6 @@
+#Copyright (c) Microsoft. All rights reserved.
+#Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
 include("${CMAKE_CURRENT_LIST_DIR}/testrunnerswitcherTargets.cmake")
 
 get_target_property(TESTRUNNERSWITCHER_INCLUDES testrunnerswitcher INTERFACE_INCLUDE_DIRECTORIES)

1a0594a2ff0336c6aa306685da4bd3d53248ddfa
Dan Cristoloveanu <dcristo@microsoft.com>
2021-04-04 21:41:46 -0700

    Move test projects from c build tools

---
+++ build_functions/main_cpp_unittest.cpp
@@ -0,0 +1,7 @@
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+#include "run_cppunittest_suite.h"
+
+// test_suite_name is a define passed from the CMakeLists and identifies the test suite name
+RUN_TESTS_WITH_CPP_UNITTEST(TEST_SUITE_NAME_FROM_CMAKE)
---
+++ test_projects/test_project_ut/main.c
@@ -0,0 +1,8 @@
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+int main(void)
+{
+    // this unit does nothing, it is just an empty executable to test generation of test projects
+    return 0;
+}

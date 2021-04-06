f8e65cff4d8c4ccefbb79d8ddb3324845912fdd6
Dan Cristoloveanu <dcristo@microsoft.com>
2021-04-06 16:21:57 -0700

    Have a build_test_artifacts_with_custom_main so that we do not have to have the boilerplate code for main.c all over the place

--- test_projects/test_project_perf/main.c
+++
@@ -1,8 +0,0 @@
-// Copyright (c) Microsoft. All rights reserved.
-// Licensed under the MIT license. See LICENSE file in the project root for full license information.
-
-int main(void)
-{
-    // this unit does nothing, it is just an empty executable to test generation of test projects
-    return 0;
-}

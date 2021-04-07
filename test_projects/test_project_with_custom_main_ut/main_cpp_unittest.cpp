65b0522b48295bc8db260a5db8b932a0c06d863d
Dan Cristoloveanu <dcristo@microsoft.com>
2021-04-06 17:11:18 -0700

    Have a build_test_artifacts_with_custom_main so that we do not have to have the boilerplate code for main.c all over the place (#66)
    * Have a build_test_artifacts_with_custom_main so that we do not have to have the boilerplate code for main.c all over the place
    
    * Address CR comments

--- test_projects/test_project_with_custom_main_ut/main_cpp_unittest.cpp
+++ test_projects/test_project_with_custom_main_ut/main_cpp_unittest.cpp
@@ -1,8 +1,4 @@
 // Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-int main(void)
-{
-    // this unit does nothing, it is just an empty executable to test generation of test projects
-    return 0;
-}
+const int dummy = 42;
